07855a56b2b7294136eba424da0f8822c21c9002
Alex <perevedko@perevedko>
2024-04-30 22:13:55 +0400

    added a new endpoint

--- app.rb
+++ app.rb
@@ -7,12 +7,29 @@ Bundler.require :default
 class Application < Sinatra::Base
   get '/', provides: :json do
     {
-      available_endpoints: ['GET /', 'GET /fast', 'GET /file.json']
+      available_endpoints: [
+        {
+          path: 'GET /',
+          description: 'the root'
+        },
+        {
+          path: 'GET /fast',
+          description: ''
+        },
+        {
+          path: 'GET /file.json',
+          description: 'is it constant??'
+        },
+        {
+          path: 'GET /nestness?n=<integer>',
+          description: 'what is it?'
+        }
+      ]
     }.to_json
   end
 
   get '/fast' do
-    sleep 5
+    sleep 3
     'is it?'
   end
 
@@ -21,6 +38,20 @@ class Application < Sinatra::Base
       foo: { bar: { baz: :foooo }},
       rand: rand(1..100)
     }.to_json
+  end
+
+  get '/nestness', provides: :json do
+    n = params[:n].to_i || 10
+    response =
+      if n <= 0
+        'whoops!'
+      else
+        opening = '[' * n
+        center = JSON::dump("why?") if n >= 100
+        closing = ']' * n
+        JSON.parse("#{opening}#{center}#{closing}")
+      end
+    JSON::dump(response)
   end
 end
 
